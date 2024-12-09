5d9e6626611dd74834de5a06b539be2036e36d67
robertinpopescu <111278117+robertinpopescu@users.noreply.github.com>
2024-12-09 17:50:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Funcție pentru a deschide un overlay specific
+function openNav(id) {
+    document.getElementById(id).style.height = "100%";
+}
+
+// Funcție pentru a închide un overlay specific
+function closeNav(id) {
+    document.getElementById(id).style.height = "0%";
+}
